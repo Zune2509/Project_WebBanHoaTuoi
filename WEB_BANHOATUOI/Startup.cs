9f28437c238648f659a6f07bffbb182564fefdde
Zune <ngchidung2105@gmail.com>
2023-04-09 23:34:15 +0700

    Add project files.

---
+++ WEB_BANHOATUOI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WEB_BANHOATUOI.Startup))]
+namespace WEB_BANHOATUOI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
